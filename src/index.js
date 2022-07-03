06c319d80421cb61d2dedc417eb24eee2a836dcb
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-03 17:50:53 +0100

    Little formatting done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-// 1. Import the React and ReactDom libraries
+// 1. Import the React and ReactDom libraries.
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-// 2. Get a reference to the div with ID root
+// 2. Get a reference to the div with ID root.
 const el = document.getElementById('root');
 
-// 3. Tell React to tak control of that element
+// 3. Tell React to tak control of that element.
 const root = ReactDOM.createRoot(el);
 
-// 4. Create a component
+// 4. Create a component.
 function App() {
   const name = 'Courtney'
   const age = 28
@@ -19,5 +19,5 @@ function App() {
   </h1>;
 }
 
-//5. Show the component on the screen
+//5. Show the component on the screen.
 root.render(<App />)
