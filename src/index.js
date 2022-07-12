2b06ac8bcf61d8230f10e95c5d9f6dab2566a5f3
Courtney Stow <courtney-stow@hotmail.co.uk>
2022-07-12 19:07:46 +0100

    Was given a little piece of code to correct mistakes than have been mad in jsx

--- src/index.js
+++ src/index.js
@@ -9,9 +9,34 @@ const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
 // 4. Create a component
+
+// function App() {
+//   return (
+//     <div class="wrapper">
+//       <textarea                            <-- Was given this code with mistakes to correct.
+//         readonly="true"                        Below is the correct layout for jsx.
+//         maxlength="3"
+//         spellcheck="true"
+//         style="background-color: gray;"
+//       />
+//     </div>
+//   );   
+// }
+
 function App() {
-  return <input style={{border: '1px solid blue'}}/>
+  return (
+    <div className="wrapper">
+      <textarea
+        readOnly
+        maxLength={3}
+        spellCheck
+        style={{backgroundColor: "gray"}}
+      />
+    </div>
+  );   
 }
+
+export default App;
 
 //5. Show the component on the screen 
 root.render(<App />)
